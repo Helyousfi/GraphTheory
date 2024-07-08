09accf428a3c09d98f268b18191288f4f8b8847f
helyousfi <visioncomputer200@gmail.com>
2024-07-08 21:03:48 +0200

    Leaf sum added

--- src/LeafSum.cpp
+++ src/LeafSum.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include "Tree.hpp"
-
-
-
 int LeafSum(TreeNode* node)
 {
-
-}
-
-bool isLeaf(TreeNode* node)
-{
-
+    if(node->children.size() == 0) // isLeaf
+        return node->val;
+    int total = 0;
+    for(TreeNode* nodeIterator : node->children)
+    {
+        total += LeafSum(nodeIterator);
+    }
+    return total;
 }
