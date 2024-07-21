422917488a03cf351e65b751df6d574f437af9e7
helyousfi <visioncomputer200@gmail.com>
2024-07-21 02:25:07 +0200

    Some cleaning

--- GraphLibrary/Utilss/debug.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef DEBUG_FUNCTIONS_HPP
-#define DEBUG_FUNCTIONS_HPP
-#include "../../include/graph/adjacency_matrix_graph.hpp"
-
-// Function to print debugging information about the graph
-void debugGraph(const AdjacencyMatrixGraph* graph);
-
-#endif // DEBUG_FUNCTIONS_HPP
